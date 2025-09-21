0a0adc7a60921eae3737e5f60f657f775cf12239
jihun pyo <pyo636324@gmail.com>
2025-09-21 11:49:48 +0900

    0,1주차 실습 인증

---
+++ practice/chapter01/practice01.ts
@@ -0,0 +1,11 @@
+// string 성공 , 실패 실습
+// const str: string=123;
+// const myname : string ="표표";
+
+// number 성공, 실패 실습
+// const age : number = "24";
+// const age : number =24;
+
+// boolean 성공, 실패 실습 
+// const boolean : "표표" =true;
+//const 표표: boolean=true;
