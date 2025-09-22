e9be88aa68d7ddbe70c4e5d866b828cee8d76d54
jihun pyo <pyo636324@gmail.com>
2025-09-22 19:26:16 +0900

    1주차 미션인증

--- practice/chapter01/practice01.ts
+++ practice/chapter01/practice01.ts
@@ -9,3 +9,65 @@
 // boolean 성공, 실패 실습 
 // const boolean : "표표" =true;
 //const 표표: boolean=true;
+
+//null의 사용법 성공, 실패
+
+// const username: string=null;
+
+// let username:string | null=null;
+
+//undefined 사용법, 성공, 실패
+// let age :number = undefined;
+// let age : number | undefined;
+
+//symbol 사용법 성공, 실패
+// const sym1 : symbol = Symbol("표표");
+// const id : symbol = Symbol("id");
+
+// const myname : symbol = "표표";
+
+//bigint 사용법 성공, 실패
+
+// const a : bigint=123n;
+// const b : bigint = BigInt(51351);
+
+// const c : bigint =123;
+// const d : const x=100n+20;
+
+//object 성공 실패
+// const obj1:object={name: "표표" , age : 24};
+// const obj2:object=()=>console.log("hello world");
+
+// const obj3: object =123;
+// const obj4 : object= "hello";
+
+//타입스크립트에만 존재하는 타입
+
+//any
+// let value: any =123;
+// value="표표";
+// value="true";
+//전부가능
+
+//unknown
+
+// let value : unknown ="hello";
+
+// console.log(value.toUpperCase());
+// if(typeof value==="string"){
+//     console.log(value.toUpperCase());
+// }
+
+// //void
+// function myname(mn:string):void{
+// 	console.log(mn);
+// }
+
+// //never
+// function thorwError(message: string):never{
+// 	throw new Error(message);
+// }
+
+// function infiniteLoop(): never{
+// 	while(true){}
+// }
