9a3421cc0b10a9834944f3db2c5d115b2045b0fa
최지원 <cjw@choejiwon-ui-noteubug.local>
2025-09-25 06:49:15 +0900

    mission chapter02 과제 인증

---
+++ mission/chapter02/mission1/src/components/Todo.tsx
@@ -0,0 +1,35 @@
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import { TodoContext, useTodo } from "../context/TodoContext";
+
+const Todo = () => {
+
+    const {todos, completeTodo, addTodo, deleteTodo, doneTodos} = useTodo();
+
+
+      
+    return (
+    <div className='todo-container'>
+        <h1 className='todo-container__header'>YONG TODO</h1>
+        <TodoForm/>
+        <div className='render-container'>
+        <TodoList 
+        title = '할 일' 
+        todos={todos} 
+        buttonLabel='완료'
+        buttonColor='#28a745'
+        onClick={completeTodo}
+        />
+        <TodoList 
+        title = '완료' 
+        todos={doneTodos} 
+        buttonLabel='삭제'
+        buttonColor='#dc3545'
+        onClick={deleteTodo}
+        />
+        </div>
+        </div>
+    );
+};
+
+export default Todo;
