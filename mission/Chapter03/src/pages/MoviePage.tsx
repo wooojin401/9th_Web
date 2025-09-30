7304f961ee47dcc8b66322eafe4f27c73eeede69
using6843@gmail.com <using6843@gmail.com>
2025-10-01 05:55:29 +0900

    mission: 3주차 미션1

---
+++ mission/Chapter03/src/pages/MoviePage.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, type ReactElement, useState } from "react";
+import { type MovieRes, type Movie } from "../types/Movie";
+import MovieCard from "../components/MovieCard";
+
+export default function MoviePage(): ReactElement {
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect((): void => {
+        const fetchMovies = async (): Promise<void> => {
+            const { data } = await axios.get<MovieRes>("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", {
+                headers: {
+                    Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`
+                }
+            });
+
+            setMovies(data.results)
+        };
+
+        fetchMovies();
+    }, []);
+
+    return (
+        <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+        lg:grid-cols-5 xl:grid-cols-6'>
+            {movies &&
+                movies.map((movie): ReactElement => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+        </div>
+    );
+};
