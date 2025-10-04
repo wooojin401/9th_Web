dcdc5d789e8a9a491f5a7d7b018a5014882649be
aeongiing <ywjk0515@gmail.com>
2025-10-04 22:40:57 +0900

    comment: mission01 수정

--- mission/chapter03/mission01/src/pages/MoviePage.tsx
+++ mission/chapter03/mission01/src/pages/MoviePage.tsx
@@ -7,31 +7,31 @@ export default function MoviePage() {
     const [movies, setMovies] = useState<Movie[]>([]);
 
     useEffect((): void => {
-        const fetchMovies = async (): Promise<void> => {
-          const { data } = await axios.get<MovieResponse>(
-            `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
-            {
-              headers: {
-                  Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
-              },
-            }
-          );
+      const fetchMovies = async (): Promise<void> => {
+        const { data } = await axios.get<MovieResponse>(
+          `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+          {
+            headers: {
+                Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+            },
+          }
+        );
 
-          console.log(data);
+        console.log(data);
 
-          setMovies(data.results);
-        };
+        setMovies(data.results);
+      };
 
-        fetchMovies();
+      fetchMovies();
     }, []);
 
     return (
-        <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4
-        lg:grid-cols-5 xl:grid-cols-6'>
-            {movies &&
-                movies.map((movie)=> (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))}
-        </div>
+      <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+      lg:grid-cols-5 xl:grid-cols-6'>
+        {movies &&
+            movies.map((movie)=> (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+      </div>
     );
 };
