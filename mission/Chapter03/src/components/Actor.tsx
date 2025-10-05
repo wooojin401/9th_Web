f080a246e69f11fed66dfc14f34c22f602b9b406
wupe1001 <sangwu1104@naver.com>
2025-10-05 21:00:08 +0900

    mission : 3항 연산자 추가 피드백 반영

--- mission/Chapter03/src/components/Actor.tsx
+++ mission/Chapter03/src/components/Actor.tsx
@@ -10,16 +10,16 @@ interface ActorProps {
 export default function Actor({ actor }: ActorProps) {
   return (
     <div className="flex flex-col items-center mb-6 w-24">
-      {!actor.profile_path && (
-        <img src={seoro} className="rounded-full size-20 object-cover"></img>
-      )}
+      <img
+        src={
+          actor.profile_path
+            ? `https://image.tmdb.org/t/p/w92${actor.profile_path}`
+            : seoro
+        }
+        alt={`${actor.name} 프로필 이미지`}
+        className="rounded-full size-20 object-cover border-2"
+      />
 
-      {actor.profile_path && (
-        <img
-          src={`https://image.tmdb.org/t/p/w92${actor.profile_path}`}
-          className="rounded-full size-20 object-cover border-2"
-        ></img>
-      )}
       <div className="text-center mt-5">
         <span className="block">{actor.name}</span>
         <span className="text-sm text-gray-400 max-w-xl line-clamp-1">
