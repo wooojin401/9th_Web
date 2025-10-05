090dce8d8a328a06bb3dff6f811f67400266737f
chxxwn <alscodnjs040418@naver.com>
2025-10-05 19:48:32 +0900

    mission: 3주차 미션3 제출

---
+++ mission/chapter_03/mission3/src/App.tsx
@@ -0,0 +1,32 @@
+import type { ReactElement } from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import MoviePage from "./pages/MoviePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "movies/:category",
+        element: <MoviePage />,
+      },
+      {
+        path: "movie/:movieId",
+        element: <MovieDetailPage />,
+        errorElement: <NotFoundPage />,
+      },
+    ],
+  },
+]);
+
+function App(): ReactElement {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
