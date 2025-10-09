ea48bef1db31d18bc1ba7a269ad84bf7660ec1e2
qkrdmsthff <126754457+qkrdmsthff@users.noreply.github.com>
2025-10-09 11:23:24 +0900

    mission : chapter04 홈페이지 수정

--- mission/chapter04_2/src/pages/HomePage.tsx
+++ mission/chapter04_2/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
     return (
         <div className="flex h-dvh bg-black text-center justify-center items-center text-pink-500 font-bold text-[80px]">
-            하이룽다 ㅋㅋ
+            치치의 LP 페이지
         </div>
     )
 }
