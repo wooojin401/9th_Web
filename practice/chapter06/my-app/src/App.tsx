811d69911f6578a861925eddaa43c5ef8c168fba
unknown <wooojin401@gmail.com>
2025-11-06 23:40:49 +0900

    practice: 6주차 연습 인증

---
+++ practice/chapter06/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { WelcomeData } from "./components/WelcomeData";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React Query Example ✅</h2>
+      <WelcomeData />
+    </div>
+  );
+}
+
+export default App;
